084692443a1cf22e67a438d9cb4242b97e43647c
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2023-07-23 21:30:58 +0200

    ⬆ UPDATED: DarkMode and Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import './App.css'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainRoutes from "./routes/MainRoutes";
+import ErrorScreen from "./pages/ErrorScreen";
 
 function App() {
 
-  return (<h1>Welcome</h1>)
+  const [darkMode, setDarkMode] = useState(true)
+
+  const changeDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+  useEffect(() => {
+    document.body.classList.remove("app-mode-dark", "app-mode-light");
+    document.body.classList.add(`app-mode-${darkMode ? "dark" : "light"}`);
+  }, [darkMode]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<MainRoutes changeDarkMode={changeDarkMode} darkMode={darkMode} />} />
+        <Route path="*" element={<ErrorScreen />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
