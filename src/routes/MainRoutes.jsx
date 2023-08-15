d0f9b7c15a2f8e3df7c2e0acb0150602466b8439
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2023-08-15 21:58:30 +0200

    ADDED: Scroll effect

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,8 +1,9 @@
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import ErrorScreen from "../pages/ErrorScreen";
 import HomeScreen from "../pages/HomeScreen";
+import AboutAccordion from "../components/AboutAccordion";
 import PropTypes from "prop-types";
 
 const MainRoutes = ({changeDarkMode, darkMode}) => {
@@ -11,6 +12,7 @@ const MainRoutes = ({changeDarkMode, darkMode}) => {
 				<Navbar changeDarkMode={changeDarkMode} darkMode={darkMode}/>
 				<Routes>
 					<Route path="/" element={<HomeScreen changeDarkMode={changeDarkMode} darkMode={darkMode}/>} />
+					<Route path="/about" element={<AboutAccordion changeDarkMode={changeDarkMode} darkMode={darkMode}/>} />
 					<Route path="*" element={<ErrorScreen changeDarkMode={changeDarkMode} darkMode={darkMode}/>} />
 				</Routes>
 				<Footer changeDarkMode={changeDarkMode} darkMode={darkMode}/>
